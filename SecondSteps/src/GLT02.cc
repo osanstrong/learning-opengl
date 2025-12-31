efeead3eff4a1786086e8818dfa9581c8501e4d6
Owen Strong <owensanstrong@gmail.com>
2025-12-30 19:09:44 -0600

    Added blank black window

--- SecondSteps/src/GLT02.cc
+++ SecondSteps/src/GLT02.cc
@@ -2,12 +2,41 @@
  * Following tutorial at https://learnopengl.com/Getting-started
  */
 
-#include <GLFW/glfw3.h>
+#include <GL/gl.h> // gl.h first to avoid typedef issues
+#include <GLFW/glfw3.h> //
 #include <iostream>
 #include <stb_image.hh>
 
 
 int main(int argc, char** argv) {
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(400, 300, "Window Test", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    
+    // Viewport adjustment
+    glViewport(0, 0, 400, 300);
+    auto framebuffer_size_callback = [](GLFWwindow* window, int width, int height) {
+        glViewport(0, 0, width, height);
+    };  
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     std::cout << "Program launched successfully\n";
     return 0;
 }
